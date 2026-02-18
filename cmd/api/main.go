09bb91afe853de4dacb5c2052c80692b61f4a70e
DotNicolasPenha <nicolaspenha45@gmail.com>
2026-02-18 17:33:23 -0300

    feat: in post module: service

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,22 +2,23 @@ package main
 
 import (
 	"github.com/DotNicolasPenha/Posts-CRUD/internal/database"
+	"github.com/DotNicolasPenha/Posts-CRUD/internal/http"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
 	// database postgresql configurations and connection
 	connectionString := "postgresql://posts:12345@db:5432/posts"
-	_, err := database.NewConnection(connectionString)
+	conn, err := database.NewConnection(connectionString)
 	if err != nil {
 		panic(err)
 	}
+	defer conn.Close()
+
 	// api configurations and route /
 	g := gin.Default()
-	g.GET("/", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "Hello World",
-		})
-	})
+	http.SetupRoutes(g)
 	g.Run(":3000")
+
 }
---
+++ internal/modules/post/service.go
@@ -0,0 +1,26 @@
+package post
+
+import "errors"
+
+type Service struct {
+	repository Repository
+}
+
+func NewService(r *Repository) *Service {
+	if r == nil {
+		panic("repository is nil")
+	}
+	return &Service{repository: *r}
+}
+func (s *Service) AddPost(post Post) error {
+	if post.Username == "" {
+		return errors.New("the username of post is empty")
+	}
+	if post.Body == "" {
+		return errors.New("the body of post is empty")
+	}
+	if err := s.repository.Insert(post); err != nil {
+		return err
+	}
+	return nil
+}

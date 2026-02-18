06deeec9c86bfae9221d9cccb2b6d22035915656
DotNicolasPenha <nicolaspenha45@gmail.com>
2026-02-18 17:32:44 -0300

    feat: module post

---
+++ internal/modules/post/entity.go
@@ -0,0 +1,14 @@
+package post
+
+import (
+	"time"
+
+	uuid "github.com/jackc/pgtype/ext/gofrs-uuid"
+)
+
+type Post struct {
+	ID        uuid.UUID `json:"-"`
+	Username  string    `json:"username"`
+	Body      string    `json:"body"`
+	CreatedAt time.Time `json:"created_at"`
+}

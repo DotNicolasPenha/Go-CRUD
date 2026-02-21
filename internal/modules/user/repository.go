c43eebdf9660a231283818236ea6c4243021d58d
DotNicolasPenha <nicolaspenha45@gmail.com>
2026-02-21 17:11:27 -0300

    feat: user repository

---
+++ internal/modules/user/repository.go
@@ -0,0 +1,32 @@
+package post
+
+import (
+	"context"
+	"time"
+
+	"github.com/DotNicolasPenha/Posts-CRUD/internal/common/logger"
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+type Repository struct {
+	conn *pgxpool.Pool
+}
+
+func NewRepository(conn *pgxpool.Pool) *Repository {
+	if conn == nil {
+		logger.Fatal("connection of repository user is nil")
+	}
+	return &Repository{
+		conn: conn,
+	}
+}
+
+func (r *Repository) Insert(createUserDto CreateUserDTO) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	_, err := r.conn.Exec(
+		ctx, "INSERT INTO users (nameuser,bio) VALUES ($1,$2)", createUserDto.Username, createUserDto.Bio,
+	)
+	return err
+}
